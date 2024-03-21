9271c68a3c23b90efc672d2579d8be56f911eac4
aanasakhtar <154276028+aanasakhtar@users.noreply.github.com>
2024-03-22 00:07:35 +0500

    Create helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,19 @@
+def create_a_linkedlist():
+    return {}
+
+def insert_a_tuple(linked_list, ID, status, data, connection = None):
+    linked_list[ID] = (status, data, connection)
+
+def remove_node(linked_list, ID):
+    for c in linked_list:
+        if linked_list[c][2] == ID:
+            linked[c] = (linked[c][0], linked[c][1], None)
+    tup = linked_list[ID]
+    del linked_list[ID]
+    return tup
+
+def length_of_linkedlist(linked_list):
+    return len(linked_list.keys())
+
+def print_linked_list(linked_list):
+    pass # returns the head of the list. 
