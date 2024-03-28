8dd5fcd3f42cd8e99b5eb975c0f92d96f80cbe1d
vishalrajsundrani <104062771+vishalrajsundrani@users.noreply.github.com>
2024-03-28 12:05:37 +0500

    Created A Basic Layout

--- main.py
+++ main.py
@@ -26,7 +26,31 @@ tools = 5
 rect_speed = 5
 
 
-box_color = OFF_WHITE
+# Importing Images
+
+battery_img = pygame.image.load('resources/images/battery.png')
+battery_img_size = battery_img.get_size()
+scaled_battery_img = pygame.transform.scale(battery_img, (battery_img_size[0]*0.08, battery_img_size[1]*0.08))
+
+bulbOn_img = pygame.image.load('resources/images/bulb_on.png')
+bulbOff_img = pygame.image.load('resources/images/bulb_off.png')
+bulb_img_size = bulbOn_img.get_size()
+scaled_bulbOn_img = pygame.transform.scale(bulbOn_img, (bulb_img_size[0]*0.15,bulb_img_size[1]*0.15))
+scaled_bulbOff_img = pygame.transform.scale(bulbOff_img, (bulb_img_size[0]*0.15,bulb_img_size[1]*0.15))
+
+
+# Menu Button
+menuIcon_img = pygame.image.load('resources\images\menu_icon.png')
+menuIcon_img_size = menuIcon_img.get_size()
+scaled_menuIcon_img = pygame.transform.scale(menuIcon_img, (menuIcon_img_size[0]*0.2, menuIcon_img_size[1]*0.2))
+scaled_menuIcon_img_size = scaled_menuIcon_img.get_size()
+menu_btn_x = WINDOW_WIDTH*0.01
+menu_btn_y = WINDOW_HEIGHT*0.1
+menu_btn_close_x = WINDOW_WIDTH*0.01
+menu_btn_open_x = WINDOW_WIDTH*0.15
+menu_open = False
+
+
 
 # Main game loop
 while True:
@@ -40,9 +64,17 @@ while True:
     keys = pygame.key.get_pressed()
     mouse = pygame.mouse.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
+
+
+
+    # Interactivity of menu button
     
-    # if mouse[0] and mouse_pos[0] >= tools_x and mouse_pos[1] >= tools_y and mouse_pos[0] <= rect_x + rect_width and mouse_pos[1] <= rect_y + rect_width:
-    #     box_color = RED
+    if mouse[0] and mouse_pos[0] >= menu_btn_x and mouse_pos[1] >= menu_btn_y and mouse_pos[0] <= menu_btn_x + scaled_menuIcon_img_size[0] and mouse_pos[1] <= menu_btn_y + scaled_menuIcon_img_size[1]:
+        menu_open = not menu_open
+        if menu_btn_x == menu_btn_open_x:
+            menu_btn_x = menu_btn_close_x
+        elif menu_btn_x == menu_btn_close_x:
+            menu_btn_x = menu_btn_open_x
 
 
 
@@ -54,17 +86,34 @@ while True:
     # Fill the background
     window.fill(OFF_WHITE)
 
+
+    #Draw menu button
+
+
+
+    # Draw battary
+    window.blit(scaled_battery_img, (WINDOW_WIDTH*0.8,WINDOW_HEIGHT*0.6))
+
+
     # Draw the side element tab
-    pygame.draw.rect(window, LIGHT_BLUE, (0, 70, WINDOW_WIDTH*0.2, WINDOW_HEIGHT-70))
+    if menu_open:
+        pygame.draw.rect(window, LIGHT_BLUE, (0, 70, WINDOW_WIDTH*0.2, WINDOW_HEIGHT-70), border_top_right_radius = 50)
+        window.blit(scaled_menuIcon_img, (menu_btn_x, menu_btn_y))
+
+        # Draw Bulb on Tab
+        window.blit(scaled_bulbOn_img, (WINDOW_WIDTH*0.05, ((WINDOW_HEIGHT-70)/(tools+1))*1))
 
 
-    # Draw Elements Tab
-    for i in range(tools):
-        pygame.draw.rect(window, OFF_WHITE, (WINDOW_WIDTH*0.07, (WINDOW_HEIGHT/(tools+1))*(i+1), tools_width, tools_height))
+    else:
+        pygame.draw.rect(window, LIGHT_BLUE, (0, 70, WINDOW_WIDTH*0.06, WINDOW_HEIGHT-70), border_top_right_radius = 50)
+        window.blit(scaled_menuIcon_img, (menu_btn_close_x, menu_btn_y))
+
+
+    
 
 
     # Update the display
     pygame.display.update()
 
     # Cap the frame rate
-    pygame.time.Clock().tick(60)
+    pygame.time.Clock().tick(5)
